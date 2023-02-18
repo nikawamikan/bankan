52bb30cdd9fce01a1c3c866f8424d6666fc3f26f
nikawamikan <wkwkwtktkkrkr@outlook.jp>
2023-02-18 16:21:38 +0900

    apiの追加

--- api/main.py
+++ api/main.py
@@ -1,11 +1,15 @@
-from datetime import datetime
-from pathlib import Path
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 from model.image import get_image
 app = FastAPI()
 
 
 @app.get("/{char_name:str}")
-async def genshin_photo(char_name: str, text: str):
-    return
+async def get_char_image(char_name: str,  message: str):
+    if char_name == "bankan":
+        data = await get_image(image_id=30, message=message)
+    elif char_name == "gomi":
+        data = await get_image(image_id=30, message=message)
+    else:
+        raise ValueError("is not a valid")
+    return StreamingResponse(content=await data.get_discord_file(), media_type="png")
